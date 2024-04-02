6d670bc3584b13e9ebbd73171c8891ded2559721
kdando <kvdando@gmail.com>
2024-04-02 13:55:40 +0100

    basic quiz script up and running, spanish vocab only

--- __main__.py
+++ __main__.py
@@ -1,9 +1,35 @@
+import random
+
+from spanish_vocab import spanish_words as words
+
 def greet_user(name):
-    print(f"Hello, {name}!")
+    print(f"Hello, {name}! Welcome to the Lil Language Learner.\nThis app will help you learn the 100 most common words of your chosen language - a solid basis to actually communicate. You will be asked 10 questions at a time.\nPress Enter to start the quiz.")
 
 name = input("What's your name?\n")
 
 greet_user(name)
+
+score = 0
+language = words
+
+def run_quiz(score, language):
+
+    while score >= 0 and score < 5:
+        current_word = random.choice(words)
+        spanish_word = list(current_word.keys())[0]
+        english_translation = current_word[spanish_word]
+
+        users_answer = input(f"What does {spanish_word} mean?\n")
+
+        if users_answer in english_translation:
+            score += 1
+            print(f"Correct! Your current score is {score}.")
+        else:
+            score -= 1
+            print(f"Incorrect. '{spanish_word}' means '{english_translation[0]}'.\nYour current score is {score}.")
+            
+
+run_quiz(score, language)
 
 
 
