4e5c2eefbffcc68739c1b3a9535d6f18147b295d
kdando <kvdando@gmail.com>
2024-04-02 20:27:32 +0100

    basic flask app structure setup and simple quiz functionality

--- __main__.py
+++ __main__.py
@@ -1,45 +1,50 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from spanish_vocab import spanish_words as words
 import random
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return render_template("index.html")
+#####################FUNCTIONS
+
+def greet_user(username):
+    return f"Hello, {username}! Welcome to the Lil Language Learner.\nThis app will help you learn the 100 most common words of your chosen language - a solid basis to actually communicate!"
+
+def set_question():
+    current_word = random.choice(words)
+    spanish_word = list(current_word.keys())[0]
+    english_translation = current_word[spanish_word]
+    return spanish_word, english_translation
+
+def run_quiz(spanish_word, english_translation, answer):
+    if answer in english_translation:
+        return "Correct!"
+    else:
+        return f"Incorrect :( '{spanish_word}' means '{english_translation[0]}'."
+
+####################ROUTES
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    username = None
+    if request.method == "POST":
+        username = request.form.get("username")
+        return render_template("quiz.html")
+    return render_template("index.html",username=username)
+
+@app.route("/quiz", methods=["POST"])
+def quiz():
+    spanish_word, english_translation = set_question()
+    return render_template('quiz.html', greeting=greet_user(request.form.get("username")), spanish_word=spanish_word, english_translation=english_translation)
+
+@app.route("/result", methods=["POST"])
+def result():
+    answer = request.form.get("answer")
+    spanish_word = request.form.get("spanish_word")
+    english_translation = request.form.get("english_translation")
+    judgement = run_quiz(spanish_word, english_translation, answer)
+    return render_template("result.html", result=judgement, spanish_word=spanish_word, english_translation=english_translation)
 
 #####################
-
-# def greet_user(name):
-#     print(f"Hello, {name}! Welcome to the Lil Language Learner.\nThis app will help you learn the 100 most common words of your chosen language - a solid basis to actually communicate. You will be asked 10 questions at a time.\nPress Enter to start the quiz.")
-
-# name = input("What's your name?\n")
-
-# greet_user(name)
-
-# score = 0
-# language = words
-
-# def run_quiz(score, language):
-
-#     while score >= 0 and score < 5:
-#         current_word = random.choice(words)
-#         spanish_word = list(current_word.keys())[0]
-#         english_translation = current_word[spanish_word]
-
-#         users_answer = input(f"What does {spanish_word} mean?\n")
-
-#         if users_answer in english_translation:
-#             score += 1
-#             print(f"Correct! Your current score is {score}.")
-#         else:
-#             score -= 1
-#             print(f"Incorrect. '{spanish_word}' means '{english_translation[0]}'.\nYour current score is {score}.")
-
-
-# run_quiz(score, language)
-
-#####
 
 if __name__ == '__main__':
     app.run(debug=True)
